fc67bafec8da62e770b5cc387b9947ee5a99cff4
njferrari <njferrari14@gmail.com>
2020-04-20 08:42:09 -0500

    finished styles, added functionality on form submit, cleaned up code

--- scripts.js
+++ scripts.js
@@ -3,37 +3,59 @@ let fields = document.querySelectorAll('input');
 let gottem = document.getElementById('r4');
 let button = document.getElementById('button');
 let form = document.querySelector('form');
+let header = document.querySelector('h1');
 let validZip = /^\d{5}$|^\d{5}-\d{4}$/;
 let validName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
 let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
-//set listener on submit button
-button.addEventListener('click', function(event) {
-	//loop through all input elements and validate
+setListeners();
+
+function setListeners() {
+	//loop to set event listeners on each input element
 	for (i = 0; i < fields.length; i++) {
-		let type = fields[i].getAttributeNode('type').value;
+		let field = fields[i];
+		let type = field.getAttributeNode('type').value;
+		//add listeners to every input
+		//except the radio and submit buttons
 		if ((type != 'submit') && (type != 'radio')) {
-			checkInput(fields[i], fields[i].value);
+			field.addEventListener('change', function(event) {
+				checkInput(field, field.value);
+				isComplete();
+			}, false);
+		}
+		if ((type != 'submit') && (type != 'radio')) {
+			field.addEventListener('focusout', function(event) {
+				checkInput(field, field.value);
+				isComplete();
+			}, false);
 		}
 	}
-}, false);
-
-//loop to set event listeners on each input element
-for (i = 0; i < fields.length; i++) {
-	setTabListeners(fields[i]);
+	//set listener on submit button
+	button.addEventListener('click', function(event) {
+		//loop through all input elements and validate
+		for (i = 0; i < fields.length; i++) {
+			let type = fields[i].getAttributeNode('type').value;
+			if ((type != 'submit') && (type != 'radio')) {
+				checkInput(fields[i], fields[i].value);
+			}
+		}
+	}, false);
+	//set listener for form on submit
+	form.addEventListener('submit', function(event) {
+		handleForm(event);
+	}, false);
 }
 
-//set event listener for current element
-function setTabListeners(field) {
-	let type = field.getAttributeNode('type').value;
-	//add focusout listeners to every input
-	//except the radio and submit buttons
-	if ((type != 'submit') && (type != 'radio')) {
-		field.addEventListener('focusout', function(event) {
-			checkInput(field, field.value);
-			isComplete();
-		}, false);
-	}
+//when form is submitted, do this
+function handleForm(event) {
+	//stop page refresh
+	event.preventDefault();
+	//disable the form
+	form.style.display = 'none';
+	header.innerText = 'Thank you!';
+	header.style.marginTop = '200px';
+	//say thank you
+
 }
 
 function checkInput(field, input) {
@@ -43,7 +65,7 @@ function checkInput(field, input) {
 	//if there is no input, make warning visible
 	//else validate input
 	if (((input === '') && (type != 'radio')) || ((type === 'checkbox') && (!field.checked))) {
-		field.style.border = '2px inset red';
+		field.style.border = '3px outset red';
 		field.nextElementSibling.className += ' visible';
 		field.nextElementSibling.classList.remove('done');
 	} else if ((type != 'radio') && (type != 'checkbox') && (type != 'submit')) {
@@ -72,11 +94,11 @@ function validate(field, input, id) {
 	//if invalid, display correct warning, 
 	//else remove warning classes and turn border green
 	if (!isValid) {
-		field.style.border = '2px inset red';
+		field.style.border = '3px outset red';
 		field.nextElementSibling.classList.remove('done');
 		field.nextElementSibling.className = 'warning hidden ' + id + '-warning';
 	} else {
-		field.style.border = '2px inset green';
+		field.style.border = '3px inset green';
 		field.nextElementSibling.className = 'warning hidden done ';
 	}
 }
@@ -98,10 +120,14 @@ function getAge(DOB) {
 function isComplete() {
 	let finishedFields = document.querySelectorAll('.done');
 	if (finishedFields.length === 6) {
-		form.style.borderColor = 'green';
+		form.style.border = '8px outset green';
 	} else {
 		form.style.borderColor = 'yellow';
 	}
+}
+
+function thankYou() {
+	form.style.display = 'none';
 }
 
 //this is just for fun

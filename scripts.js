3c052d01d3cc60ca97fdca3359ee7189f7c42048
njferrari <njferrari14@gmail.com>
2020-04-19 21:15:47 -0500

    got submit button validation working

--- scripts.js
+++ scripts.js
@@ -1,17 +1,28 @@
 //initialize variables
 let fields = document.querySelectorAll('input');
 let gottem = document.getElementById('r4');
+let button = document.getElementById('button');
 let validZip = /^\d{5}$|^\d{5}-\d{4}$/;
 let validName = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
 let validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
 
 //loop to set event listeners on each field
 for (i = 0; i < fields.length; i++) {
-	setListener(fields[i]);
+	setTabListeners(fields[i]);
 }
 
+//set listener on submit button
+button.addEventListener('click', function(event) {
+	for (i = 0; i < fields.length; i++) {
+		let type = fields[i].getAttributeNode('type').value;
+		if ((type != 'submit') && (type != 'radio')) {
+			checkInput(fields[i], fields[i].value);
+		}
+	}
+}, false);
+
 //set event listener for current element
-function setListener(field) {
+function setTabListeners(field) {
 	let type = field.getAttributeNode('type').value;
 	//add focusout listeners to every input
 	//except the radio and submit buttons
@@ -20,26 +31,18 @@ function setListener(field) {
 			checkInput(field, field.value);
 		}, false);
 	}
-	//this is just for fun
-	window.addEventListener('click', function(event) {
-		if (gottem.checked) {
-			gottem.nextElementSibling.className = 'visible gottem';
-		} else {
-			gottem.nextElementSibling.className = 'hidden';
-		}
-	}, false);
 }
 
 function checkInput(field, input) {
 	//get value of attribute node type for current element
-	let type = event.target.getAttributeNode('type').value;
-	let id = event.target.getAttributeNode('id').value
+	let type = field.getAttributeNode('type').value;
+	let id = field.getAttributeNode('id').value
 	//if there is no input, make warning visible
 	//else validate input
-	if ((input === '') || ((type === 'checkbox') && (!field.checked))) {
+	if (((input === '') && (type != 'radio')) || ((type === 'checkbox') && (!field.checked))) {
 		field.style.border = '2px dashed red';
 		field.nextElementSibling.className += ' visible';
-	} else if ((type != 'radio') && (type != 'checkbox')) {
+	} else if ((type != 'radio') && (type != 'checkbox') && (type != 'submit')) {
 		validate(field, input, id);
 	} else if ((type === 'checkbox') && field.checked) {
 		field.nextElementSibling.className = 'warning hidden ';
@@ -54,7 +57,6 @@ function validate(field, input, id) {
 		isValid = validZip.test(input);
 	} else if (id === 'email') {
 		isValid = validEmail.test(input);
-		console.log(isValid);
 	} else if (id === 'bday') {
 		let age = getAge(input);
 		if (age >= 18) {
@@ -85,6 +87,12 @@ function getAge(DOB) {
     return age;
 }
 
-function gotEm() {
 
-}
+//this is just for fun
+window.addEventListener('click', function(event) {
+	if (gottem.checked) {
+		gottem.nextElementSibling.className = 'visible gottem';
+	} else {
+		gottem.nextElementSibling.className = 'hidden';
+	}
+}, false);
